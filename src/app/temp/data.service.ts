4e1f7aceec60f06908aa2a2b6d9e7af7aa1c97ee
Joseph Kibira <33089347+97joseph@users.noreply.github.com>
2024-12-23 08:14:19 +0300

    Update data.service.ts

--- src/app/temp/data.service.ts
+++ src/app/temp/data.service.ts
@@ -68,6 +68,84 @@ const contacts: IContact[] = [
     "lastViewed": new Date("2024-11-12 06:55:25"),
     "phone": "+254712098765",
     "deleted": false
+  },
+  {
+    "id": "3f9c45e9-9a65-4af6-95c6-9b1022c1b5b1",
+    "firstName": "Fatuma",
+    "lastName": "Hassan",
+    "email": "fatuma.hassan@example.com",
+    "address": "P.O. Box 1122, Mombasa, Kenya",
+    "favorite": false,
+    "group": "friends",
+    "image": "https://placeimg.com/800/600/people",
+    "lastViewed": new Date("2024-03-15 10:20:45"),
+    "phone": "+254734567890",
+    "deleted": false
+  },
+  {
+    "id": "7a8d4e6f-4321-4d9b-84e5-75c7396a8baf",
+    "firstName": "Kevin",
+    "lastName": "Omondi",
+    "email": "kevin.omondi@example.com",
+    "address": "P.O. Box 3344, Kisii, Kenya",
+    "favorite": true,
+    "group": "work",
+    "image": "https://placekitten.com/400/400",
+    "lastViewed": new Date("2024-08-25 08:45:30"),
+    "phone": "+254799876543",
+    "deleted": false
+  },
+  {
+    "id": "914c58f7-1d6e-4bf3-b7a1-d6c4d7c0a9e1",
+    "firstName": "Linet",
+    "lastName": "Wanjiru",
+    "email": "linet.wanjiru@example.com",
+    "address": "P.O. Box 5566, Nyeri, Kenya",
+    "favorite": false,
+    "group": "family",
+    "image": "https://placeimg.com/500/500/nature",
+    "lastViewed": new Date("2024-07-10 14:12:50"),
+    "phone": "+254701112233",
+    "deleted": false
+  },
+  {
+    "id": "8b2f4a78-3c7d-4a5f-910e-2134b6d3d6f7",
+    "firstName": "Samuel",
+    "lastName": "Okoth",
+    "email": "samuel.okoth@example.com",
+    "address": "P.O. Box 7788, Nairobi, Kenya",
+    "favorite": true,
+    "group": "others",
+    "image": "https://placekitten.com/450/550",
+    "lastViewed": new Date("2024-11-20 11:00:00"),
+    "phone": "+254733445566",
+    "deleted": false
+  },
+  {
+    "id": "6c8f7a9b-4a5f-9b0c-2d7e-5f8g1h9i2j3k",
+    "firstName": "Grace",
+    "lastName": "Nyaboke",
+    "email": "grace.nyaboke@example.com",
+    "address": "P.O. Box 9900, Thika, Kenya",
+    "favorite": false,
+    "group": "friends",
+    "image": "https://placeimg.com/640/480/people",
+    "lastViewed": new Date("2024-12-15 13:30:25"),
+    "phone": "+254722334455",
+    "deleted": false
+  },
+  {
+    "id": "ab9f8e7c-1d2b-3c4d-5e6f-7g8h9i0j1k2l",
+    "firstName": "John",
+    "lastName": "Karanja",
+    "email": "john.karanja@example.com",
+    "address": "P.O. Box 2233, Machakos, Kenya",
+    "favorite": true,
+    "group": "work",
+    "image": "https://placekitten.com/512/512",
+    "lastViewed": new Date("2024-06-05 09:15:45"),
+    "phone": "+254725667788",
+    "deleted": false
   }
 ];
 
